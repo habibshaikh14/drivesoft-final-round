14660b6f93c3d0039f6059b2a38808b2d9b1de51
Habib Shaikh <164193900+habibshaikhdsj@users.noreply.github.com>
2024-11-15 20:38:47 +0530

    Spring Application Initialized

---
+++ src/main/java/com/example/drivesoft/DrivesoftApplication.java
@@ -0,0 +1,13 @@
+package com.example.drivesoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrivesoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrivesoftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/drivesoft/DrivesoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.drivesoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrivesoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
