6af34f54c174745cddef1d7ba08ee43ddccf193c
Habib Shaikh <33100185+habibshaikh14@users.noreply.github.com>
2024-11-16 18:24:30 +0530

    - improved login response

--- src/main/java/com/example/drivesoft/auth/AuthController.java
+++ src/main/java/com/example/drivesoft/auth/AuthController.java
@@ -3,6 +3,7 @@ package com.example.drivesoft.auth;
 import com.example.drivesoft.user.User;
 import com.example.drivesoft.user.UserDetailsServiceImpl;
 import com.example.drivesoft.utils.JwtUtil;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -16,31 +17,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 public class AuthController {
 
-  private final UserDetailsServiceImpl userDetailsService;
   private final JwtUtil jwtUtil;
   private final AuthenticationManager authenticationManager;
 
   public AuthController(
-          UserDetailsServiceImpl userDetailsService,
           JwtUtil jwtUtil,
           AuthenticationManager authenticationManager) {
-    this.userDetailsService = userDetailsService;
     this.jwtUtil = jwtUtil;
     this.authenticationManager = authenticationManager;
   }
 
-  @PostMapping("/addNewUser")
-  public String addNewUser(@RequestBody User userInfo) {
-    return userDetailsService.addUser(userInfo);
-  }
-
   @PostMapping("/login")
-  public String authenticateAndGetToken(@RequestBody LoginRequestDTO authRequest) {
+  public ResponseEntity<LoginResponseDTO> authenticateAndGetToken(@RequestBody LoginRequestDTO authRequest) {
     Authentication authentication = authenticationManager.authenticate(
             new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword())
     );
     if (authentication.isAuthenticated()) {
-      return jwtUtil.generateToken(authRequest.getUsername());
+      LoginResponseDTO loginResponseDTO = new LoginResponseDTO();
+      loginResponseDTO.setUsername(authRequest.getUsername());
+      loginResponseDTO.setToken(jwtUtil.generateToken(authRequest.getUsername()));
+      return ResponseEntity.ok(loginResponseDTO);
     } else {
       throw new UsernameNotFoundException("Invalid user request!");
     }
